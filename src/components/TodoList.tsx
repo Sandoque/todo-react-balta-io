2aa7b20b7bf0d553574c32310257346a1839cef0
Sandoque <acsandoque@gmail.com>
2020-10-24 15:37:14 -0300

    TodoList

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Todolist = () => {
+  return (<div>Todo List</div>);
+}
+
+export default Todolist;
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './components/App';
-import Navbar from './components/Navbar';
 
 ReactDOM.render(
   <React.StrictMode>
