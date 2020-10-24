70b5b9d9a50c5a80f09ab1054f749323371efcf2
Sandoque <acsandoque@gmail.com>
2020-10-24 16:05:35 -0300

    classe todo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Todo } from '../models/Todo';
 
 const Todolist = () => {
-  return (<div>Todo List</div>);
+  const todos: Todo[] = [
+    { id: 1, title: 'ir ao supermercado', done: false },
+    { id: 2, title: 'ir a barraca do bolo', done: false }
+  ];
+  return (<div>{todos[1].title}</div>);
 }
 
 export default Todolist;
