0d12f019f291911392f5ea70298eef04a2648d06
Sandoque <acsandoque@gmail.com>
2020-11-04 05:02:14 -0300

    form e etc

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import Navbar from './Navbar';
 import TodoList from './TodoList';
-import TodoContext from './../contexts/TodoContext';
+import TodoContext from '../contexts/TodoContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AddTodo from './AddTodo';
+
+//npm install react-router-dom @types/react-router-dom
+//yarn add react-router-dom
+//npm install react-hook-form yup @types/yup @hookform/resolvers
 
 const App = () => {
   return (
     <TodoContext>
-      <div className="uk-container">
+      <Router>
         <Navbar></Navbar>
-        <TodoList></TodoList>
-      </div>)
+        <br />
+        <div className="uk-container">
+          <Switch>
+            <Route path="/create">
+              <AddTodo></AddTodo>
+            </Route>
+            <Route path="/">
+              <h4>Minha lista de tarefas</h4>
+              <TodoList></TodoList>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </TodoContext>
   );
 }
